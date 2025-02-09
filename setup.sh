1fc8152bda2f14df296ca8506c808476d1d0d53d
MinSung Kim <min.sung.kim@intel.com>
2025-02-09 21:48:14 +0000

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python -m pip install langchain==0.0.218
+python -m pip install llama-index==0.7.21
+python -m pip install llama_hub==0.0.19
+
+python -m pip install transformers
+python -m pip install sentence_transformers
+python -m pip install sentencepiece==0.2.0
